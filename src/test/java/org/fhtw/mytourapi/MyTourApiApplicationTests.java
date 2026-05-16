a59f447603ee6aabd70b301ed29d897cdafe0284
tarik-yilmaz <tarik.yilmaz.email@gmail.com>
2026-05-16 11:33:18 +0200

    configure Spring Boot test properties to exclude specific auto-configurations

--- src/test/java/org/fhtw/mytourapi/MyTourApiApplicationTests.java
+++ src/test/java/org/fhtw/mytourapi/MyTourApiApplicationTests.java
@@ -3,7 +3,13 @@ package org.fhtw.mytourapi;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+        "spring.autoconfigure.exclude=" +
+                "org.springframework.boot.data.jpa.autoconfigure.DataJpaRepositoriesAutoConfiguration," +
+                "org.springframework.boot.flyway.autoconfigure.FlywayAutoConfiguration," +
+                "org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration," +
+                "org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration"
+})
 class MyTourApiApplicationTests {
 
     @Test
